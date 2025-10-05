e258c8635355ec18ddfe9d7b59002ab586a1c3cc
hieu <lamtrunghieuq@gmail.com>
2025-10-05 14:48:07 +0700

    phan user

--- src/main/java/trunghieu/spring_boot/entity/User.java
+++ src/main/java/trunghieu/spring_boot/entity/User.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name="users")
 public class User {
-    //main
+    //main1
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
